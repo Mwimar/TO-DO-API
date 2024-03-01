6fc06bde081ef36f506fc58d3451f886881203a2
Mark Mwima <markmwimar@gmail.com>
2024-03-02 01:53:25 +0300

    saving

--- models/todo.model.js
+++ models/todo.model.js
@@ -1,6 +1,11 @@
+const db = require("../data/database");
+
 class Todo {
   constructor(text) {
     this.text;
+  }
+  save() {
+    return db.getDb().collection("todos").insertOne({ text: this.text });
   }
 }
 
