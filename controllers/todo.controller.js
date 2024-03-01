3208b392b88d27f75d0ef35b9dceefe273a36504
Mark Mwima <markmwimar@gmail.com>
2024-03-02 02:22:34 +0300

    todo controller functions

---
+++ controllers/todo.controller.js
@@ -0,0 +1,14 @@
+function getAllTodos() {}
+
+function addTodo() {}
+
+function updateTodo() {}
+
+function deleteTodo() {}
+
+module.exports = {
+  getAllTodos: getAllTodos,
+  addTodo: addTodo,
+  updateTodo: updateTodo,
+  deleteTodo: deleteTodo,
+};
