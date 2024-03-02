10ad2f406cb3ed8c243c9f6d103998aa39ba8271
Mark Mwima <markmwimar@gmail.com>
2024-03-02 23:18:14 +0300

    get todos route

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const todosController = require("../controllers/todo.controller");
+
 const router = express.Router();
+
+router.get("/", todosController.getAllTodos);
 
 module.exports = router;
